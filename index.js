91cb5c89e916890cbc22e6d7ea3203875220c10f
Aimar Abimayu Pratama <aimarabimanyu123@gmail.com>
2024-03-04 20:44:31 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import numberAdd from "split-add";
+
+console.log(numberAdd(1234));
+console.log(numberAdd(5678));
+console.log(numberAdd(15698));
+console.log(numberAdd(747));
+console.log(numberAdd(96));
